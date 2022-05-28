c170b09edc7ee4adb44aca19815fad630070653f
Xuan Huy Le <xhuy.le@gmail.com>
2022-05-28 11:15:09 +0200

    Initial commit.

---
+++ app-demo/src/main/java/org/springframework/cloud/discovery/appdemo/AppDemoApplication.java
@@ -0,0 +1,12 @@
+package org.springframework.cloud.discovery.appdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDemoApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(AppDemoApplication.class, args);
+    }
+}
---
+++ eureka/src/main/java/org/springframework/cloud/discovery/eureka/EurekaApplication.java
@@ -0,0 +1,14 @@
+package org.springframework.cloud.discovery.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
