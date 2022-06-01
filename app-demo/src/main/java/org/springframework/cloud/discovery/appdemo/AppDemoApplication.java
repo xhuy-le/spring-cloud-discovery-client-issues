656679aceb6bc87ddfe799e8d4fac77d7deba8da
Xuan Huy Le <xhuy.le@gmail.com>
2022-06-01 13:27:29 +0200

    Add some logs to display registered discovery clients.

--- app-demo/src/main/java/org/springframework/cloud/discovery/appdemo/AppDemoApplication.java
+++ app-demo/src/main/java/org/springframework/cloud/discovery/appdemo/AppDemoApplication.java
@@ -1,12 +1,21 @@
 package org.springframework.cloud.discovery.appdemo;
 
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AppDemoApplication {
 
     public static void main(final String[] args) {
         SpringApplication.run(AppDemoApplication.class, args);
+    }
+
+    @Bean
+    public String configServerInstanceProvider(final ObjectProvider<DiscoveryClient> discoveryClient) {
+        discoveryClient.stream().map(DiscoveryClient::toString).forEach(System.out::println);
+        return "test";
     }
 }
